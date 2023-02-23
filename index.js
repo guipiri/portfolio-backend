67b344de13f811aae34d52380c2d3738c98f24ed
Guilherme Oliveira <gui.soliveiras@gmail.com>
2023-02-23 14:53:54 -0300

    criando rota para subir arquivos html para o servidor

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 
-const upload = multer({ dest: "uploads/" });
-
 const port = process.env.PORT || 3001;
+const upload = multer({ dest: "uploads/" });
 const app = express();
-// app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	app.use(cors());
+	next();
+});
 
-app.post("/uploadFile", upload.single("htmlInput"), (req, res) => {
-	console.log(req.file);
+app.post("/uploadFile", upload.single("htmlInput"), async (req, res) => {
+	if (req.file) {
+		res.status(200, "aoiahao");
+		res.send("O upload ocorreu com sucesso!");
+		res.status;
+	} else {
+		res.send("Ocorreu uma falha no seu upload!");
+	}
+
 	res.end();
 });
 
